0173a354dbd223857ee7a010bc75520641dc9d27
selfstrong <542363450@qq.com>
2016-03-31 21:19:15 +0800

    消息表的javabean

---
+++ HumanResourceSys/src/com/bit/common/PovinceDateQuery.java
@@ -0,0 +1,68 @@
+package com.bit.common;
+
+public class PovinceDateQuery {
+	private String com_name;
+	private String ID;
+	private String user_type;
+	private String area;
+	private String com_property;
+	private String start_m;
+	private String start_y;
+	private String end_m;
+	private String end_y;
+	public String getCom_name() {
+		return com_name;
+	}
+	public void setCom_name(String com_name) {
+		this.com_name = com_name;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getUser_type() {
+		return user_type;
+	}
+	public void setUser_type(String user_type) {
+		this.user_type = user_type;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCom_property() {
+		return com_property;
+	}
+	public void setCom_property(String com_property) {
+		this.com_property = com_property;
+	}
+	public String getStart_m() {
+		return start_m;
+	}
+	public void setStart_m(String start_m) {
+		this.start_m = start_m;
+	}
+	public String getStart_y() {
+		return start_y;
+	}
+	public void setStart_y(String start_y) {
+		this.start_y = start_y;
+	}
+	public String getEnd_m() {
+		return end_m;
+	}
+	public void setEnd_m(String end_m) {
+		this.end_m = end_m;
+	}
+	public String getEnd_y() {
+		return end_y;
+	}
+	public void setEnd_y(String end_y) {
+		this.end_y = end_y;
+	}
+	
+}
