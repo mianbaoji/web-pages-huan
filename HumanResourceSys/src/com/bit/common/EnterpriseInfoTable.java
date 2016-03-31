efc63225a4b3cecd8131042e45749b1fdfee15ad
JaZeFe <JaZeFe@163.com>
2016-03-31 11:42:26 +0800

    省用户企业用户整合v1.0
    省用户界面与企业用户端第一次整合

---
+++ HumanResourceSys/src/com/bit/common/EnterpriseInfoTable.java
@@ -0,0 +1,90 @@
+package com.bit.common;
+
+public class EnterpriseInfoTable {
+	private String com_id;
+	private String com_area;
+	private String com_name;
+	private String com_property;
+	private String com_industry;
+	private String com_business;
+	private String com_people;
+	private String com_address;
+	private String com_postalcode;
+	private String com_tel;
+	private String com_fax;
+	private String com_email;
+	public String getCom_id() {
+		return com_id;
+	}
+	public void setCom_id(String com_id) {
+		this.com_id = com_id;
+	}
+	public String getCom_area() {
+		return com_area;
+	}
+	public void setCom_area(String com_area) {
+		this.com_area = com_area;
+	}
+	public String getCom_name() {
+		return com_name;
+	}
+	public void setCom_name(String com_name) {
+		this.com_name = com_name;
+	}
+	public String getCom_property() {
+		return com_property;
+	}
+	public void setCom_property(String com_property) {
+		this.com_property = com_property;
+	}
+	public String getCom_industry() {
+		return com_industry;
+	}
+	public void setCom_industry(String com_industry) {
+		this.com_industry = com_industry;
+	}
+	public String getCom_business() {
+		return com_business;
+	}
+	public void setCom_business(String com_business) {
+		this.com_business = com_business;
+	}
+	public String getCom_people() {
+		return com_people;
+	}
+	public void setCom_people(String com_people) {
+		this.com_people = com_people;
+	}
+	public String getCom_address() {
+		return com_address;
+	}
+	public void setCom_address(String com_address) {
+		this.com_address = com_address;
+	}
+	public String getCom_postalcode() {
+		return com_postalcode;
+	}
+	public void setCom_postalcode(String com_postalcode) {
+		this.com_postalcode = com_postalcode;
+	}
+	public String getCom_tel() {
+		return com_tel;
+	}
+	public void setCom_tel(String com_tel) {
+		this.com_tel = com_tel;
+	}
+	public String getCom_fax() {
+		return com_fax;
+	}
+	public void setCom_fax(String com_fax) {
+		this.com_fax = com_fax;
+	}
+	public String getCom_email() {
+		return com_email;
+	}
+	public void setCom_email(String com_email) {
+		this.com_email = com_email;
+	}
+
+	
+}
---
+++ HumanResourceSys/src/com/bit/common/TimeTable.java
@@ -0,0 +1,26 @@
+package com.bit.common;
+
+public class TimeTable {
+	private String time_id;
+	private String time_start;
+	private String time_end;
+	
+	public String getTime_id() {
+		return time_id;
+	}
+	public void setTime_id(String time_id) {
+		this.time_id = time_id;
+	}
+	public String getTime_start() {
+		return time_start;
+	}
+	public void setTime_start(String time_start) {
+		this.time_start = time_start;
+	}
+	public String getTime_end() {
+		return time_end;
+	}
+	public void setTime_end(String time_end) {
+		this.time_end = time_end;
+	}
+}
